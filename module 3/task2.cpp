4940dde6e1d0208e721078a80fd05ffffdfccab7
Artem Zhuk <artem.zx656@gmail.com>
2023-12-24 19:27:47 +0300

    reduced complexity by replacing priority_queue with queue

--- module 3/task2.cpp
+++ module 3/task2.cpp
@@ -7,16 +7,6 @@
 #include <queue>
 #include <climits>
 
-struct PairComp {
-    constexpr bool operator()(
-            std::pair<int, int> const &a,
-            std::pair<int, int> const &b)
-    const noexcept {
-        return a.second > b.second;
-    }
-};
-
-
 class Graph {
 public:
     Graph(std::size_t vertices_count) {
@@ -31,40 +21,20 @@ public:
         return vertices.size();
     }
 
-    std::vector<int> GetNextVertices(int vertex) const {
-        std::vector<int> result;
-        for (int v: vertices[vertex]) {
-            result.push_back(v);
-        }
-        return result;
-    }
-
-    std::vector<int> GetPrevVertices(int vertex) const {
-        std::vector<int> result;
-
-        for (int from = 0; from < vertices.size(); ++from) {
-            for (int to: vertices[from]) {
-                if (to == vertex) {
-                    result.push_back(from);
-                }
-            }
-        }
-        return result;
-    }
-
     int CountNumOfDifferentWays(int u, int w) {
         std::vector<int> dist(VerticesCount(), INT_MAX);
         std::vector<int> path_counter(VerticesCount(), 0);
-        std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, PairComp> queue;
+        std::queue<std::pair<int, int>> queue;
         path_counter[u] = 1;
         dist[u] = 0;
         queue.emplace(u, 0);
+
         while (!queue.empty()) {
-            int popped = queue.top().first;
-            int distance_to_popped = queue.top().second;
+            int popped = queue.front().first;
+            int distance_to_popped = queue.front().second;
             queue.pop();
 
-            if(distance_to_popped >= dist[popped]){
+            if (distance_to_popped >= dist[popped]) {
                 for (int &neighbour: vertices[popped]) {
                     if (dist[neighbour] > dist[popped] + 1) {
                         dist[neighbour] = dist[popped] + 1;
